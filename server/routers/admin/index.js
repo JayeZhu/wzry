f4d806acf5f5b89fe3d199dc1cb92873241569d4
JayeZhu <1032657951@qq.com>
2020-07-23 22:59:48 +0800

    移动端样式和主页顶部

--- server/routers/admin/index.js
+++ server/routers/admin/index.js
@@ -53,14 +53,7 @@ module.exports = app => {
     res.send(model);
   });
 
-  app.use('/admin/api/rest/:resource', authMiddleware(), resourceMiddleware(), async (req, res, next) => {
-    // 添加中间件，动态获取到模型
-    // 通过 inflection 的 classfy 将 从父级获取的参数 转为大写单数，即 模型名
-    const modelName = require('inflection').classify(req.params.resource);
-    // 动态获取模型,将其挂在在 req 上，表明请求这个模型
-    req.Model = require(`../../models/${modelName}`);
-    next(); // 执行下一步
-  }, router);
+  app.use('/admin/api/rest/:resource', authMiddleware(), resourceMiddleware(), router);
 
   // 导入multer，建立文档中间件 upload
   const multer = require('multer');
