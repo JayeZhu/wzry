316ce310ab625b98ff56bc777c166301237b6335
unknown <F1334606@systemplus.foxconn.com>
2020-09-03 19:15:14 +0800

    chonglai

--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import './style.scss'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 new Vue({
   router,
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    
+    <swiper :options="swiperOptions">
+      <swiper-slide><img class="w-100" src="../../public/images/210794580bb9303653804bb7b482f2a4.jpeg" alt="" srcset=""></swiper-slide>
+      <swiper-slide><img class="w-100" src="../../public/images/ddc8c8922cbb694dfb73c86bb03fce22.jpeg" alt="" srcset=""></swiper-slide>
+      <swiper-slide><img class="w-100" src="../../public/images/b9905b35bb0afa9050d9ddbe04d82d29.jpeg" alt="" srcset=""></swiper-slide>
+      <div class="swiper-pagination pagination-home" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
@@ -8,6 +13,28 @@
 
 export default {
   name: 'Home',
-  
+  data: () => ({
+    swiperOptions: {
+      pagination: {
+        el: '.pagination-home'
+      }
+    }
+  })
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../style.scss';
+>>> .swiper-pagination-bullet {
+  background-color: #fff !important;
+}
+.swiper-pagination {
+  .swiper-pagination-bullet {
+    background-color: #fff!important;
+    &.swiper-pagination-bullet-active {
+      background-color: map-get($colors, 'info') !important;
+    }
+  }
+  text-align: right
+}
+</style>
